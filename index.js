719b00ef934893a34208baf44db15c0a24457496
Nicolás Salazar <nicolas@ncodes.dev>
2021-05-02 16:30:54 -0500

    Configured node project base

---
+++ index.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+console.time("Execution time");
+console.log(chalk.white.bold(`Started index.js 🥸`));
+
+console.log(chalk.white.bold(`Finished index.js 🧐`));
+console.timeEnd("Execution time");
