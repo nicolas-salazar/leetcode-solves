d898d08042294879499c8b2d0e9fd892b4cf9984
Nicolás Salazar <nicolas@ncodes.dev>
2021-05-02 16:39:34 -0500

    Added cool and colored console logs to main file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const chalk = require('chalk');
 console.time("Execution time");
 console.log(chalk.white.bold(`Started index.js 🥸`));
+// Aquí empieza mi código c:
 
+// Aquí termina...
 console.log(chalk.white.bold(`Finished index.js 🧐`));
 console.timeEnd("Execution time");
